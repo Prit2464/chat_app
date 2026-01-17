e7c3c893efcf16b2d9242d55cb833a84207355b6
Prit2464 <pritwazurkar24@gmail.com>
2026-01-17 19:14:17 +0530

    signup Fix database connection issue

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,6 +5,9 @@ dotenv.config()
 
 export const connectDB = async () => {
     try {
+
+        const { MONGODB_URI } = process.env
+        if (!MONGODB_URI) throw new Error("MONGODB_URI is not set")
         const conn = await mongoose.connect(process.env.MONGODB_URI)
         console.log("Host ", conn.connection.host)
     } catch (error) {
